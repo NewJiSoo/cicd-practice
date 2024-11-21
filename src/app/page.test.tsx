cee5eb3ff022e18f58cecfd8c5fca2ccfa207105
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2024-11-21 10:34:53 +0900

    test

---
+++ src/app/page.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Home from "./page";
+import "@testing-library/jest-dom";
+
+describe("Page", () => {
+  it("renders the page", () => {
+    render(<Home />);
+    expect(screen.getByText("Get started by editing")).toBeInTheDocument();
+  });
+});
